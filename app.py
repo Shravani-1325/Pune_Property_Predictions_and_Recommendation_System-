39de404434149531b41f354d9b3a5e3df85d09b6
Shravani-1325 <moreshravani93@gmail.com>
2026-02-25 00:22:59 +0530

    updated app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ import json
 import plotly.express as px
 import plotly.graph_objects as go
 import joblib
-import fontstyle
 
 st.set_page_config(page_title="Pune Property Intelligence", layout="wide")
 
